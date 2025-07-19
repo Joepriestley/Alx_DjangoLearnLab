8835182ef04c058afd9c0ed2323475ad923fd5f2
Joepriestley <joeamonoo1114@gmail.com>
2025-07-19 16:19:05 +0100

    created __str__ for name

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name =models.CharField(max_length=100)
+
 class Book(models.Model):
     title= models.CharField(max_length=150)
     author= models.ForeignKey(Author,on_delete=models.CASCADE)
@@ -14,3 +15,5 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library =models.OneToOneField(Library, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"{self.name}"
