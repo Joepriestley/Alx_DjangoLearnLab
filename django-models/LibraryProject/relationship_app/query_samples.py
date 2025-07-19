b41a5985d912629e2a5f967af1a123175a695992
Joepriestley <joeamonoo1114@gmail.com>
2025-07-19 17:42:58 +0100

    query_sampls.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,6 @@
 from relationship_app.models import  Book,Librarian, Library, Author
 # Query all books by a specific author
-author=Author.objects.gets(name="author_name")
+author=Author.objects.get(name="author_name")
 book=Book.objects.filter(author=author)
 # Listing all books in a library.
 library = Library.objects.get(name=library_name)
