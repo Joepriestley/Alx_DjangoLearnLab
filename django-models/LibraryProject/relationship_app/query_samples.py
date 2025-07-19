508ecdc0ce337378475013d862d46e477721c66f
Joepriestley <joeamonoo1114@gmail.com>
2025-07-19 17:41:45 +0100

    query_sampless.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from relationship_app.models import  Book,Librarian, Library, Author
 # Query all books by a specific author
-book=Book.objects.all(author="George Orwell")
-
+author=Author.objects.gets(name="author_name")
+book=Book.objects.filter(author=author)
 # Listing all books in a library.
 library = Library.objects.get(name=library_name)
 library.books.all()
