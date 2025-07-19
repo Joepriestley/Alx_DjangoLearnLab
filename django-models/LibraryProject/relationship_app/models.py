53e283f0fe48a06292d44f63151f188ea843716f
Joepriestley <joeamonoo1114@gmail.com>
2025-07-19 16:27:22 +0100

    created __str__

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,11 +9,11 @@ class Book(models.Model):
     author= models.ForeignKey(Author,on_delete=models.CASCADE)
 class Library(models.Model):
     name= models.CharField(max_length=100) 
-    books= models.ManyToManyField(Book,related_name='books')
+    books= models.ManyToManyField(Book)
     
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library =models.OneToOneField(Library, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"{self.name}"
+        return str(self.name)
