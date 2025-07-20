3697d46e6e09012c6f59e65dd4878c6a474cb193
Joepriestley <joeamonoo1114@gmail.com>
2025-07-20 23:55:20 +0100

    creat urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import list_books,LibraryDetailView
 
 
 Urlspatterns=[
-    path('books/', views.book_list),
-    path('library/', views.bookDetail.as_view())
+    path('books/', views.list_books, name='books'),
+    path('library/', views.LibraryDetailView.as_view(), name='library_detail')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,12 +6,12 @@ from .models import Library
 
 
 # Create your views here.
-def book_list(request):
+def list_books(request):
     books= Book.objects.all()
     context={'book_list':books}
     return render(request, 'relationship_app/list_books.html',context)
 #class based view to render books in library
-class bookDetail(DetailView):
+class LibraryDetailView(DetailView):
     model= Library
     template_name = 'relationship_app/library_detail.html'
     
