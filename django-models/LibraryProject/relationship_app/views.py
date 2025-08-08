46fd6eb770fcca791640d3c5f9105bd76b86af8a
Joepriestley <joeamonoo1114@gmail.com>
2025-08-08 03:56:03 +0100

    User authentication implementations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,6 @@
+
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
