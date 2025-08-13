603e1ec43e1100d7403b0984d7048428b4524e81
Joepriestley <joeamonoo1114@gmail.com>
2025-08-13 16:32:51 +0100

    permissions

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,19 +1,64 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.contenttypes.models import ContentType
 from .models import Book, CustomUser
-# Register your models here.
 
+# Custom User Admin
 class CustomUserAdmin(UserAdmin):
-    # Show these fields in the admin form
-    fieldsets=UserAdmin.fieldsets+(
-        ('Additional Information',{'fields':('date_of_birth','profile_photo')}),
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    list_display=('username','email','date_of_birth', 'is_staff')
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+
 admin.site.register(CustomUser, CustomUserAdmin)
 
 
-class BookADmin(admin.ModelAdmin):
-    list_display =('title','author','publication_year')
-    list_filter=('title',)
-    search_fields=('author','title')
-admin.site.register(Book, BookADmin)
+# Book Admin
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('title',)
+    search_fields = ('author', 'title')
+
+admin.site.register(Book, BookAdmin)
+
+
+# Automatically create groups and assign permissions
+def setup_groups_and_permissions():
+    # Get Book content type
+    book_content_type = ContentType.objects.get_for_model(Book)
+
+    # Define custom permissions
+    view_book_permission, _ = Permission.objects.get_or_create(
+        codename='can_view_books',
+        name='Can View Books',
+        content_type=book_content_type
+    )
+
+    add_book_permission = Permission.objects.get(codename='add_book')
+    change_book_permission = Permission.objects.get(codename='change_book')
+    delete_book_permission = Permission.objects.get(codename='delete_book')
+
+    # Create or update "Librarian" group
+    librarian_group, _ = Group.objects.get_or_create(name='Librarians')
+    librarian_group.permissions.set([
+        add_book_permission,
+        change_book_permission,
+        delete_book_permission,
+        view_book_permission
+    ])
+
+    # Create or update "Reader" group
+    reader_group, _ = Group.objects.get_or_create(name='Readers')
+    reader_group.permissions.set([view_book_permission])
+
+
+# Hook into Django's admin ready signal
+from django.apps import apps
+from django.db.models.signals import post_migrate
+
+def create_groups_after_migrate(sender, **kwargs):
+    if sender.name == 'your_app_name':  # Replace with your app name
+        setup_groups_and_permissions()
+
+post_migrate.connect(create_groups_after_migrate)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render,get_object_or_404
+
 
 # Create your views here.
+@permission_required('bookshelf.can_create' , raise_exception=True)
+def can_create(request):
+    #logic here 
+    pass
+    return render(request, 'bookshelf/create_book.html')
+@permission_required('bookshelf.can_edit' , raise_exception=True)
+def can_edit(request,pk):
+    book = get_object_or_404(Book,pk)
+    #logic here 
+    pass
+    return render(request, 'bookshelf/edit_book.html')
+@permission_required('bookshelf.can_delete' , raise_exception=True)
+def can_delete(request,pk):
+    book=get_object_or_404(Book, pk)
+    #logic here 
+    pass
+    return render(request, 'bookshelf/delete_book.html')
+
+@permission_required('bookshelf.can_view' , raise_exception=True)
+def can_view(request):
+    #logic here 
+    pass
+    return render(request, 'bookshelf/view.html')
