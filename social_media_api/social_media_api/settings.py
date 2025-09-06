b4137ed0117e58a9f748c58c16e4ad5e44554c56
Joepriestley <joeamonoo1114@gmail.com>
2025-09-07 00:50:47 +0100

    setting social_media_api django framework

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_mob7y*t1lz5_d4=52cyrl@&2o(wha#f%g@0*l6z7k6pdpwkp#"
+SECRET_KEY = "django-insecure-hona!j1=j+iv=%i*vhl90a3tr=)v!&eakw*u4kt694p+2ubtv6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
     "accounts",
 ]
 
